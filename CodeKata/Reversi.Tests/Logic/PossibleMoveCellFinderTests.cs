1ea67b535c9cce4db49711075f6a41b775f19b9a
appastoy <appastoy82@gmail.com>
2019-03-12 02:57:01 +0900

    Align code.

--- CodeKata/Reversi.Tests/Logic/PossibleMoveCellFinderTests.cs
+++ CodeKata/Reversi.Tests/Logic/PossibleMoveCellFinderTests.cs
@@ -20,45 +20,45 @@ namespace Reversi.Logic.Tests
 			TestGrid(CellColor.White, ".0BWB0").Should_Be_Correct();
 
 			TestGrid(CellColor.White, ".....",
-								 "..W..",
-								 ".WBW.",
-								 "..W..",
-								 ".....").Should_Be_Correct();
+									  "..W..",
+									  ".WBW.",
+									  "..W..",
+									  ".....").Should_Be_Correct();
 
 			TestGrid(CellColor.Black, "..0..",
-								 "..W..",
-								 "0WBW0",
-								 "..W..",
-								 "..0..").Should_Be_Correct();
+									  "..W..",
+									  "0WBW0",
+									  "..W..",
+									  "..0..").Should_Be_Correct();
 
 			TestGrid(CellColor.White, ".....",
-								 ".W.W.",
-								 "..B..",
-								 ".W.W.",
-								 ".....").Should_Be_Correct();
+									  ".W.W.",
+									  "..B..",
+									  ".W.W.",
+									  ".....").Should_Be_Correct();
 
 			TestGrid(CellColor.Black, "0...0",
-								 ".W.W.",
-								 "..B..",
-								 ".W.W.",
-								 "0...0").Should_Be_Correct();
+									  ".W.W.",
+									  "..B..",
+									  ".W.W.",
+									  "0...0").Should_Be_Correct();
 
 			TestGrid(CellColor.White, ".....",
-								 ".WWW.",
-								 ".WBW.",
-								 ".WWW.",
-								 ".....").Should_Be_Correct();
+									  ".WWW.",
+									  ".WBW.",
+									  ".WWW.",
+									  ".....").Should_Be_Correct();
 
 			TestGrid(CellColor.Black, "0.0.0",
-								 ".WWW.",
-								 "0WBW0",
-								 ".WWW.",
-								 "0.0.0").Should_Be_Correct();
+									  ".WWW.",
+									  "0WBW0",
+									  ".WWW.",
+									  "0.0.0").Should_Be_Correct();
 
 			TestGrid(CellColor.Black, "..0.",
-								 ".BW0",
-								 "0WB.",
-								 ".0..").Should_Be_Correct();
+									  ".BW0",
+									  "0WB.",
+									  ".0..").Should_Be_Correct();
 		}
 	}
 }
